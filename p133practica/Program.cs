9b9c8e9a6b94ffa3f7f087854079ad9457248111
cavid32 <javidhal@code.edu.az>
2022-09-07 23:24:20 +0400

    first commit

---
+++ p133practica/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace p133practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Isdifadeci trefinden cumle daxil olunur.
+            //Daxil Olunan Cumlenin Sozleri Oz Sirasinda Qalmaqla Tersine Cevrilir.
+            //Misal: Salam Qrup Uzvler-- > malaS purQ relvzU Method Seklinde Yazirsiniz
+            Console.WriteLine("Cumle daxil edin ");
+            string sentence = Console.ReadLine();
+            string[] words  = sentence.Split(" ");
+            string tersCumle = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+               char[] characters = words[i].ToCharArray();
+                for (int j = characters.Length -1; j >=0; j--)
+                {
+                    Console.Write(characters[j]);
+                }
+                Console.Write(" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
